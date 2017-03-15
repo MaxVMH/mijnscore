3c83280d110be7774743a6a25a18d2d64d568aa4
Max <maxim@iMac-van-Maxim.local>
2017-03-15 23:01:22 +0100

    more detailled
    better explanation + link to code on github

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,7 +5,9 @@ require_once '../app/views/header.php';
 <h3>Welkom bij mijnscore.be</h3>
 Eerst en vooral: mijnscore.be is een webdevelopment oefening en daarom geen echte website, game of tool. Omdat mijnscore.be nog volop in ontwikkeling is, kan het zijn dat er hier en daar iets niet werkt.<br />
 <br />
-We zijn nog volop in opbouw. Probeer de website gerust al eens uit, en <a href="http://www.9lives.be/forum/webdesign-webdevelopment/1050659-voetbalpronostiek.html">laat het weten mocht het fout lopen.</a><br />
+Momenteel wordt mijnscore.be getest door deelnemers van de 9lives voetbal pronostiek.<br />
+<br />
+Mocht het fout lopen, kan je <a href="https://www.9lives.be/forum/webdesign-webdevelopment/1050659-voetbalpronostiek.html">hier</a> of in de prono- en nabesprekingthreads een berichtje achterlaten. Mensen die geïnteresseerd zijn in de code kunnen terecht <a href="https://github.com/MaxVMH/mijnscore/">op github.</a><br />
 <br />
 
 <?php
