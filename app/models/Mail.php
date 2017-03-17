01fee71e2de7d6174fe88961c8624f641deda188
Max <maxim@iMac-van-Maxim.local>
2017-03-17 15:17:27 +0100

    added verification & notification emails
    added verification and notification email methods

--- app/models/Mail.php
+++ app/models/Mail.php
@@ -1,14 +1,12 @@
 <?php
 class Mail
 {
-	public function send_mail($receiver, $subject, $body)
+	public function send_email($receiver, $subject, $body)
 	{
 		require '../app/libraries/PHPMailer/PHPMailerAutoload.php';
 
 		$mail = new PHPMailer();
 		$mail->IsSMTP();
-
-		$mail->Debugoutput = "html";
 
 		$mail->Host = "smtp.gmail.com";
 		$mail->Port = 587;
@@ -20,19 +18,65 @@ class Mail
 		$mail->Password = GMAIL_PASS;
 
 		$mail->SetFrom(GMAIL_ADDR);
-
 		$mail->AddAddress($receiver);
 		$mail->Subject = $subject;
 		$mail->Body = $body;
 
 		$mail->IsHTML (true);
 
-		if(!$mail->send()) {
-			echo 'Message was not sent.';
-			echo 'Mailer error: ' . $mail->ErrorInfo;
-			return false;
-		} else {
+		if($mail->send())
+		{
 			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function send_verification_email($receiver_email, $receiver_username, $verification_hash)
+	{
+		$subject = "Bevestig uw e-mailadres";
+		$body = "<h3>E-mail adres bevestigen op mijnscore.be</h3>
+		<p>
+		Dag " . $receiver_username . ", <br />
+		<br />
+		Gelieve op <a href='http://mijnscore.be/users/email_verification/" .$verification_hash . "'>deze link</a> te klikken om uw e-mail adres te bevestigen. Indien u deze e-mail niet gevraagd heeft, mag u deze e-mail negeren.<br />
+		<br />
+		Met vriendelijke groeten, <br />
+		mijnscore.be
+		</p>";
+
+		if($this->send_email($receiver_email, $subject, $body))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function send_notification_email($receiver_email, $receiver_username)
+	{
+		$subject = "Herinnering van mijnscore.be";
+		$body = "<h3>Herinnering van mijnscore.be</h3>
+		<p>
+		Dag " . $receiver_username . ", <br />
+		<br />
+		Dit is een herinnering om uw pronostiek in te vullen op <a href='http://mijnscore.be'>mijnscore.be</a>.<br />
+		<br />
+		Met vriendelijke groeten, <br />
+		" . WEBSITE_TITLE . "
+		</p>";
+
+		if($this->send_email($receiver_email, $subject, $body))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
 		}
 	}
 }
