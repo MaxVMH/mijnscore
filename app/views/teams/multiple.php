046b88f8649519b4ee0e622c615fb3cc44a63883
Max <maxim@iMac-van-Maxim.local>
2017-03-18 13:44:58 +0100

    added edit links for admins
    added edit links for admins in the multiple teams view, closes #14

--- app/views/teams/multiple.php
+++ app/views/teams/multiple.php
@@ -7,6 +7,18 @@ require_once '../app/views/header.php';
 		<th>ID</th>
 		<th>Ploeg (tag)</th>
 		<th>Naam</th>
+
+		<?php
+		if($data['user_loggedin']['user_rank'] == "9")
+		{
+			?>
+
+			<th>Bewerk</th>
+
+			<?php
+		}
+		?>
+
 	</tr>
 
 	<?php
@@ -18,6 +30,18 @@ require_once '../app/views/header.php';
 			<td><?= $team['team_id']; ?></td>
 			<td><a href="teams/single/<?= $team['team_id']; ?>"><?= $team['team_tag']; ?></a></td>
 			<td><?= $team['team_name']; ?></td>
+
+			<?php
+			if($data['user_loggedin']['user_rank'] == "9")
+			{
+				?>
+
+				<td><a href="teams_admin/edit/<?= $data['team']['team_id']; ?>">Bewerk ploeg</a></td>
+
+				<?php
+			}
+			?>
+
 		</tr>
 
 		<?php
