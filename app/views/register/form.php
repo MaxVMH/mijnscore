f53e71f687635b4399dce1d58ecb64a5b5b6dc58
Maxim <Maxim@Maxim-iMac.local>
2018-06-08 16:28:31 +0200

    added information about what data mijnscore collects
    added information about what data mijnscore collects

--- app/views/register/form.php
+++ app/views/register/form.php
@@ -27,6 +27,18 @@ Eerst en vooral: mijnscore.be is een webdevelopment oefening en daarom geen echt
 			<td><input type="password" name="password_repeat" size="30" /></td>
 		</tr>
 		<tr>
+			<td colspan="2">Welke data houdt mijnscore bij?<br />
+				<ul>
+					<li>Gebruikersnaam en hash van het wachtwoord (om u te kunnen inloggen).</li>
+					<li>E-mail adres (om u e-mail herinneringen te kunnen sturen).</li>
+					<li>Het tijdstip waarop u het laatst inlogde (om inactieve accounts te kunnen verwijderen)</li>
+					<li>IP-adres van de verbinding waarmee u registreert (om misbruik tegen te gaan).</li>
+					<li>Het tijdstip waarop iemand een fout wachtwoord voor uw account ingeeft, en het aantal keren dat dit voorvalt (om misbruik tegen te gaan).</li>
+				</ul>
+				Indien u uw gegevens wilt laten aanpassen of laten verwijderen, stuur dan een berichtje naar Destel op 9lives of mijnscore.
+			</td>
+		</tr>
+		<tr>
 			<th colspan="2"><input type="submit" name="register" value="Maak nieuw account" /></th>
 		</tr>
 	</table>
